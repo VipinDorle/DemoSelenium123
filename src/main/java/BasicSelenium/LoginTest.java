91e71eb1647b0412cdfc251da1aad0ae4a9ac499
VipinDorle <dorlevipin@gmail.com>
2023-08-09 07:53:19 +0000

    Login Automated

--- src/main/java/BasicSelenium/LoginTest.java
+++ src/main/java/BasicSelenium/LoginTest.java
@@ -1,5 +1,6 @@
 package BasicSelenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,6 +10,12 @@ public class LoginTest {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://the-internet.herokuapp.com/login");		
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("tomsmith");
+		driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+		driver.findElement(By.className("radius")).click();
+		driver.findElement(By.linkText("Elemental Selenium")).click();
+		
 	}
 
 }
