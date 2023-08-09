0a100751fa8112a45a3aaff9d466b53b896cd035
VipinDorle <dorlevipin@gmail.com>
2023-08-09 07:11:19 +0000

    add webdriver

--- src/main/java/BasicSelenium/LoginTest.java
+++ src/main/java/BasicSelenium/LoginTest.java
@@ -1,10 +1,14 @@
 package BasicSelenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/login");		
 	}
 
 }
